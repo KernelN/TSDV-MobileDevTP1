9924040f89f56c043c1d616398d77dbced626d36
Alejandro Insausti <alejandroninsausti@gmail.com>
2025-09-27 02:33:16 -0300

    Add addresables & revert to old input system

---
+++ Assets/SCRIPTS/SceneManagement/SimpleAddressableInstantiator.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class SimpleAddressableInstantiator : MonoBehaviour
+{
+    [SerializeField] List<AssetReferenceGameObject> addressables;
+    List<GameObject> instances;
+
+    void Awake()
+    {
+        instances = new List<GameObject>();
+        for (int i = 0; i < addressables.Count; i++)
+            addressables[i].InstantiateAsync(transform).Completed += OnAddresableLoaded;
+    }
+    void OnDestroy()
+    {
+        for (int i = 0; i < instances.Count; i++)
+            Addressables.ReleaseInstance(instances[i]);
+    }
+    void OnAddresableLoaded(AsyncOperationHandle<GameObject> handle)
+    {
+        if (handle.Status == AsyncOperationStatus.Succeeded) 
+            instances.Add(handle.Result);
+    }
+}   
